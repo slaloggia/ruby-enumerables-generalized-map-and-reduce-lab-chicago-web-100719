57dcbfea557620bdaab387c59e8b83b47525372c
Sarah Laloggia <sarah.laloggia@gmail.com>
2019-09-24 23:37:16 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -18,7 +18,7 @@ def reduce(source_array, start_value = 0)
   
   while i < source_array.length do
     
-     yield(total, source_array[i])
+     total = yield(total, source_array[i])
       i += 1
 
   end
