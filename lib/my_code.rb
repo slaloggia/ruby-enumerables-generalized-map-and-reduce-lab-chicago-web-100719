df17924f31ab0d6350221f5a939678a7336e0b50
Sarah Laloggia <sarah.laloggia@gmail.com>
2019-09-25 00:13:42 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,32 @@
-# Your Code Here
+def map(array)
+  new = []
+  i = 0
+  while i < array.length do
+    
+    new.push(yield(array[i]))
+    i += 1 
+  end
+
+  return new
+
+end
+
+
+def reduce(source_array, start_value = 0)
+  i = 0 
+  total = start_value
+  
+  while i < source_array.length do
+    
+     total = yield(total, source_array[i])
+      i += 1
+
+  end
+  
+  if total == 0 
+    return true 
+  else
+    return total
+  end
+  
+end
